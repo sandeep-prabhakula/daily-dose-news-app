0bbb173aeb9141cb899ed3fab88c88dbd1a8f75e
sandeep <sandeepprabhakula10@gmail.com>
2022-07-14 22:03:30 +0530

    new api added

--- src/App.js
+++ src/App.js
@@ -35,10 +35,10 @@ const App = () => {
       <Router>
         <Navbar mode={mode} toggleMode={toggleMode} />
         <Routes>
-          <Route exact path='/' element={<News setProgress={changeProgress} apiKey={apiKey} key='general' pageSize={pageSize} country='in' category='general' mode={mode} />} />
+          <Route exact path='/' element={<News setProgress={changeProgress} apiKey={apiKey} key='all' pageSize={pageSize} country='in' category='all' mode={mode} />} />
           <Route exact path='/business' element={<News setProgress={changeProgress} apiKey={apiKey} key='business' pageSize={pageSize} country='in' category='business' mode={mode} />} />
           <Route exact path='/entertainment' element={<News setProgress={changeProgress} apiKey={apiKey} key='entertainment' pageSize={pageSize} country='in' category='entertainment' mode={mode} />} />
-          <Route exact path='/health' element={<News setProgress={changeProgress} apiKey={apiKey} key='health' pageSize={pageSize} country='in' category='health' mode={mode} />} />
+          <Route exact path='/politics' element={<News setProgress={changeProgress} apiKey={apiKey} key='politics' pageSize={pageSize} country='in' category='politics' mode={mode} />} />
           <Route exact path='/science' element={<News setProgress={changeProgress} apiKey={apiKey} key='science' pageSize={pageSize} country='in' category='science' mode={mode} />} />
           <Route exact path='/sports' element={<News setProgress={changeProgress} apiKey={apiKey} key='sports' pageSize={pageSize} country='in' category='sports' mode={mode} />} />
           <Route exact path='/technology' element={<News setProgress={changeProgress} apiKey={apiKey} key='technology' pageSize={pageSize} country='in' category='technology' mode={mode} />} />
--- src/components/News.js
+++ src/components/News.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
-import Spinner from './Spinner'
 import PropTypes from 'prop-types'
-import InfiniteScroll from 'react-infinite-scroll-component'
+
 
 const News = (props) => {
 
     const [articles, setArticles] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [page, setPage] = useState(1);
-    const [totalResults, setTotalResults] = useState(0);
 
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -17,31 +13,20 @@ const News = (props) => {
 
     const updateNews = async () => {
         props.setProgress(10)
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
-        setLoading(true)
+        const url = `https://inshorts.deta.dev/news?category=${props.category}`
         let data = await fetch(url)
         props.setProgress(50)
         let parseData = await data.json()
         props.setProgress(70)
-        setLoading(false)
-        setArticles(parseData.articles)
-        setTotalResults(parseData.totalResults)
+        setArticles(parseData.data)
         props.setProgress(100)
     }
-
-    const fetchMoreData = async () => {
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`
-        setPage(page + 1)
-        let data = await fetch(url)
-        let parseData = await data.json()
-        setArticles(articles.concat(parseData.articles))
-        setTotalResults(parseData.totalResults)
-    };
 
     useEffect(() => {
         document.title = `DailyDose - ${capitalizeFirstLetter(props.category)}`
         props.setProgress(0)
         updateNews()
+        // eslint-disable-next-line
     }, [])
     return (
         <div style={{
@@ -49,31 +34,23 @@ const News = (props) => {
             color: props.mode === 'light' ? '#000' : '#fff'
         }}>
             <h1 className='text-center' style={{
-                marginTop:'50px'
-            }}>Ape News - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
-            {loading && <Spinner />}
-            <InfiniteScroll
-                dataLength={articles.length}
-                next={fetchMoreData}
-                hasMore={articles.length !== totalResults}
-                loader={<Spinner />}
-            >
-                <div className='container ' >
-                    <div className="row">
-                        {articles.map((element) => {
-                            return <div key={element.url} className="col-md-4">
-                                <NewsItem title={element.title ? element.title.slice(0, 45) : ""}
-                                    description={element.description ? element.description.slice(0, 88) : ""}
-                                    imageURL={element.urlToImage ? element.urlToImage : "https://i.ytimg.com/vi/z2T9NDVpzXk/hqdefault.jpg"}
-                                    newsURL={element.url}
-                                    date={element.publishedAt}
-                                    source={!element.source.name ? "---" : element.source.name}
-                                    author={!element.author ? "---" : element.author} mode={props.mode} />
-                            </div>
-                        })}
-                    </div>
+                marginTop: '50px'
+            }}>Daily Dose - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+            <div className='container'>
+                <div className="row">
+                    {articles.map((element) => {
+                        return <div key={element.url} className="col-md-4">
+                            <NewsItem title={element.title ? element.title : ""}
+                                description={element.content ? element.content : ""}
+                                imageURL={element.imageUrl ? element.imageUrl : "https://i.ytimg.com/vi/z2T9NDVpzXk/hqdefault.jpg"}
+                                newsURL={element.url}
+                                date={element.date}
+                                source="InShorts"
+                                author={!element.author ? "---" : element.author} mode={props.mode} />
+                        </div>
+                    })}
                 </div>
-            </InfiniteScroll>
+            </div>
         </div>
     )
 }
